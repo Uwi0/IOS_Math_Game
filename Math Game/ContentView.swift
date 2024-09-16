a84bba0327fa7a139ae9585bf588ee4fda04bfcf
uwi0 <kakapoweb@gmail.com>
2024-09-16 19:02:00 +0700

    Initial Commit

---
+++ Math Game/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Math Game
+//
+//  Created by dwi prasetyo on 16/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Math Game/Math_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Math_GameApp.swift
+//  Math Game
+//
+//  Created by dwi prasetyo on 16/09/24.
+//
+
+import SwiftUI
+
+@main
+struct Math_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
